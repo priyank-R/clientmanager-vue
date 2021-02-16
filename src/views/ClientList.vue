c561c079e33f8eee395b47255df2b3d8da9d61f2
priyank-R <42401655+priyank-R@users.noreply.github.com>
2021-02-16 12:13:56 +0530

    Improved UI by using b-table in /clientlist route - Closes #6

--- src/views/ClientList.vue
+++ src/views/ClientList.vue
@@ -1,102 +1,116 @@
 <template>
 <div>
-<authorized-view />
-    <h1>All clients </h1>
+    <authorized-view />
+    <h1>All clients</h1>
     <b-container>
-        <b-row class="justify-content-center">
-            <b-col cols="4" align="center">
-                <!-- <Table :table_data="table_data" :table_headers="table_headers" >
-                    <template v-slot:property-3>
-                        <p>Service</p>
-                    </template>
-                </Table> -->
-                <table>
-                    <tr >
-                        <th v-for="item in table_headers" :key="item.name+'tableheaders'">{{item.name}}</th>
-                    </tr>
-                        <tr v-for="client in clients" :key="client.id+'tabledata'">
-                            <td @click="$router.push('/clientdetails/'+client.id)">{{client.name}}</td>
-                            <td @click="$router.push('/clientdetails/'+client.id)">{{client.info.email}}</td>
-                            <td @click="$router.push('/clientdetails/'+client.id)">{{client.info.company ? client.info.company: "-"}}</td>
-                            <td><button @click="$router.push('/addservice/'+client.id)">+</button></td> 
-                        </tr>
-                    </table>
-            </b-col>
-        </b-row>
+        <b-table striped hover :busy="isBusy" responsive="md" :fields="fields" :items="$store.getters.getClients">
+            <template #table-busy>
+                <div class="text-center text-danger my-2">
+                    <b-spinner class="align-middle"></b-spinner>
+                    <strong> Loading...</strong>
+                </div>
+            </template>
+            <template #cell(addService)="row">
+                <router-link :to="'/addservices/' + row.item.id">
+                    <b-icon-person-plus />
+                </router-link>
+            </template>
+            <template #cell(email)="row">
+                {{ row.item.info.email }}
+
+            </template>
+            <template #cell(company)="row">
+                {{ row.item.info.company ? row.item.info.company :  '-'}}
+            </template>
+
+            <template #cell(domain)="row">
+                {{ row.item.info.domain ? row.item.info.domain :  '-'}}
+            </template>
+            <template #cell(viewServices)> All </template>
+        </b-table>
     </b-container>
 </div>
 </template>
 
 <script>
-import Table from '../components/Table.vue'
+import Table from "../components/Table.vue";
+// import {
+//     store
+// } from "../store";
 import AuthorizedView from '../components/AuthorizedView.vue'
-import {getClients} from '../api/endpoints'
-import {mapGetters} from 'vuex'
+import {
+    getClients
+} from "../api/endpoints";
+import {
+    mapGetters
+} from "vuex";
+
 export default {
-    components:{Table,AuthorizedView},
-    data: function(){
-       return {
-            table_data: [
-                {name:"abc",email: 'piyurupareliya@gmail.com',domain: 'abc.com',id:2},
-                {name:"pqr",email: 'piyurupareliya@gmail.com',domain: 'abc.com',id:6,},
-                {name:"xyz",email: 'piyurupareliya@gmail.com',domain: 'abc.com', id: 9},
-                ],
-                table_headers : [
-                    {
-                        name: "Name"
-                    },
-                    {
-                        name: "Email"
-                        },
-                    {
-                        name: "Company"
-                        },
-                    {
-                        name: "Add Service",
-                        custom: true
-                    }
-                ],                
-        }
+    components: {
+        Table,
+        AuthorizedView
     },
-    computed:{
+    data: function () {
+        return {
+            isBusy: true,
+            fields: [{
+                    key: "name",
+                    label: "Name",
+                },
+                {
+                    key: "email",
+                    label: "Email",
+                },
+                {
+                    key: "domain",
+                    label: "Domain",
+                },
+                {
+                    key: "company",
+                    label: "Company",
+                },
+                {
+                    key: "addService",
+                    label: "Add Service",
+                },
+                {
+                    key: "viewServices",
+                    label: "All Services",
+                },
+            ],
+        };
+    },
+    computed: {
         ...mapGetters({
-            clients: 'getClients'
-        })
+            clients: "getClients",
+        }),
     },
-    methods:{
-        addService(clientId){
-            this.$router.push('AddService')
+    methods: {
+        addService(clientId) {
+            this.$router.push("AddService");
         },
-        getClients: async function(){
-            try{
-                console.log('GET CLIENTS CALLED')
+        getClients: async function () {
+            try {
                 const response = await getClients();
-                console.log('response inside get client : ',response)
                 return response;
-            }catch(e){
-                    console.log('error captured inside client list: ',e)
+            } catch (e) {
+                console.log("error captured inside client list: ", e);
             }
-            
-        }
+        },
     },
-    async created(){
-        console.log('created hook called insde getclients')
-        const data = this.getClients()
-        .then(response=>{
-            console.log('then invoked inside clientlist',response)
-            if(response.status=="success"){
-                    this.$store.commit('setClients',response.data.clients)
+    async created() {
+        const data = this.getClients().then((response) => {
+            if (response.status == "success") {
+                this.$store.commit("setClients", response.data.clients);
+                this.isBusy = false;
             }
-        })
-        
-    }
-    
-}
+        });
+    },
+};
 </script>
 
 <style scoped>
-tr:hover{
+tr:hover {
     background-color: rgb(219, 219, 219);
-
 }
 </style>
